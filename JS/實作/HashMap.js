f07a963f9545a7423ebaae517e9d4f012f4f4d13
Amy_01900436 <amy121232@gmail.com>
2024-08-20 16:42:58 +0800

    js第一次push

---
+++ JS/實作/HashMap.js
@@ -0,0 +1,21 @@
+var HashMap = function () {
+    let map = {};
+    return {
+        put: function (key, value) {
+            map[key] = value;
+        },
+        keys: function () {
+            let keyArr = Object.keys(map);
+            return keyArr;
+        },
+        contains: function (key) {
+            return key in map;
+        },
+        get: function (key) {
+            return map[key];
+        },
+        clear: function () {
+            map = {};
+        }
+    };
+};

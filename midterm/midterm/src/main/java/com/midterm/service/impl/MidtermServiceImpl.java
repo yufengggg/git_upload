9a20b25d2e32632a94bd12136213cba393bf62c5
Amy_01900436 <amy121232@gmail.com>
2024-08-28 16:45:06 +0800

    【final】

--- midterm/midterm/src/main/java/com/midterm/service/impl/MidtermServiceImpl.java
+++ midterm/midterm/src/main/java/com/midterm/service/impl/MidtermServiceImpl.java
@@ -127,77 +127,71 @@ public class MidtermServiceImpl implements MidtermService {
 
 	@Override
 	public Map<String, Object> metro(Map<String, String> map) {
-		/* 1. 將前端傳入值取出：使用前端傳入物件的key值，從Map中取得對應value，例如： */
+		Map<String, Object> rtnMap = new HashMap<>();
 		String startRoute = map.get("startRoute");
 		String endRoute = map.get("endRoute");
 		String startStation = map.get("startStation");
 		String endStation = map.get("endStation");
 
-		Map<String, Object> rtnMap = new HashMap<>();
-
 		try (Connection conn = dataSource.getConnection()) {
 			// 起點路線和終點路線相同
 			if (startRoute.equals(endRoute)) {
-				List<String> resultLst = calculateRouteAndFare(conn, startRoute, startStation, endStation);
+				List<String> resultLst = calculateRoute(conn, startRoute, startStation, endStation);
 				String resultString = String.join("、", resultLst);
 				int stationNum = resultLst.size(); // 經過站數
-				BigDecimal fare = new BigDecimal(stationNum).multiply(BigDecimal.TEN);
+				BigDecimal fare = new BigDecimal(stationNum).multiply(BigDecimal.TEN); // 計算車資
 
-				rtnMap.put("stationCount", stationNum); // 站點數量
+				rtnMap.put("sameRoute", true); // 判斷是否為相同路線
 				rtnMap.put("stations", resultString); // 經過的站點
+				rtnMap.put("stationCount", stationNum); // 站點數量
 				rtnMap.put("fare", fare.toPlainString()); // 車資
-				rtnMap.put("sameRoute", true);
-				// 起點路線和終點路線不同
+				
+			// 起點路線和終點路線不同
 			} else {
-				// 起始站路線可轉乘的車站
-				// 初始化需要的变量
 				List<String> preTransferStations = new ArrayList<>();
 				List<String> midTransferStations = new ArrayList<>();
 				List<String> postTransferStations = new ArrayList<>();
-				String firstTransferStation = null;
+				String firstTransferStation = null; // 第一次轉乘站
 				String firstTransferRoute = null;
 				String secondTransferStation = null;
 				String secondTransferRoute = null;
-				Boolean directTransferFound = false;
+				Boolean directTransferFound = false; // 判斷有無找到轉乘站
 
-				List<Map<String, String>> startRouteStations = findTransferStations(conn, startRoute);
+				List<Map<String, String>> startRouteStations = findTransferStations(conn, startRoute); // 找起點路線的轉乘站
 
 				for (Map<String, String> transferInfo : startRouteStations) {
 					String transferStation = transferInfo.get("station");
 					String transferRoute = transferInfo.get("transferRoute");
-
+					// 當轉乘路線跟終點路線相同
 					if (transferRoute.equals(endRoute)) {
 						firstTransferStation = transferStation;
-						// 轉乘前經過的站list
-						preTransferStations = calculateRouteAndFare(conn, startRoute, startStation,
-								firstTransferStation);
-						// 轉乘後經過的站list
-						postTransferStations = calculateRouteAndFare(conn, endRoute, firstTransferStation, endStation);
+						preTransferStations = calculateRoute(conn, startRoute, startStation, firstTransferStation);
+						postTransferStations = calculateRoute(conn, endRoute, firstTransferStation, endStation);
 
-						int stationNum = preTransferStations.size() + postTransferStations.size() - 1;
+						int stationNum = preTransferStations.size() + postTransferStations.size() - 1; // 都會包含到轉乘站
 						BigDecimal totalFare = new BigDecimal(stationNum).multiply(BigDecimal.TEN);
 
-						directTransferFound = true;
+						directTransferFound = true; // 找到轉乘站
+						
+						// 移除轉乘站在pre/post TransferStations 的資料
 						if (!preTransferStations.isEmpty() && preTransferStations.contains(firstTransferStation)) {
-						    preTransferStations.remove(firstTransferStation);
+							preTransferStations.remove(firstTransferStation);
 						}
-
-						// 确保 postTransferStations 不包含 secondTransferStation
 						if (!postTransferStations.isEmpty() && postTransferStations.contains(firstTransferStation)) {
-						    postTransferStations.remove(firstTransferStation);
+							postTransferStations.remove(firstTransferStation);
 						}
 
+						rtnMap.put("sameRoute", false);
 						rtnMap.put("preTransferStations", String.join("、", preTransferStations));
 						rtnMap.put("firstTransferStation", firstTransferStation);
 						rtnMap.put("postTransferStations", String.join("、", postTransferStations));
 						rtnMap.put("stationCount", stationNum);
 						rtnMap.put("fare", totalFare.toPlainString());
-						rtnMap.put("sameRoute", false);
 						break;
 					}
 				}
 
-				// 轉兩次車
+				// 需要轉兩次車
 				if (!directTransferFound) {
 					for (Map<String, String> transferInfo : startRouteStations) {
 						firstTransferStation = transferInfo.get("station");
@@ -212,47 +206,40 @@ public class MidtermServiceImpl implements MidtermService {
 
 							if (secondTransferRoute.equals(endRoute)) {
 
-								preTransferStations = calculateRouteAndFare(conn, startRoute, startStation,
-										firstTransferStation);
-								midTransferStations = calculateRouteAndFare(conn, firstTransferRoute,
-										firstTransferStation, secondTransferStation);
-								postTransferStations = calculateRouteAndFare(conn, secondTransferRoute,
-										secondTransferStation, endStation);
+								preTransferStations = calculateRoute(conn, startRoute, startStation, firstTransferStation);
+								midTransferStations = calculateRoute(conn, firstTransferRoute, firstTransferStation, secondTransferStation);
+								postTransferStations = calculateRoute(conn, secondTransferRoute, secondTransferStation, endStation);
 
-								int stationNum = preTransferStations.size() + midTransferStations.size()
-										+ postTransferStations.size() - 2;
+								int stationNum = preTransferStations.size() + midTransferStations.size() + postTransferStations.size() - 2;
 								BigDecimal totalFare = new BigDecimal(stationNum).multiply(BigDecimal.TEN);
 
 								directTransferFound = true;
-								
-								if (!preTransferStations.isEmpty() && preTransferStations.contains(firstTransferStation)) {
-								    preTransferStations.remove(firstTransferStation);
-								}
 
-								// 确保 midTransferStations 不包含 firstTransferStation 和 secondTransferStation
+								if (!preTransferStations.isEmpty() && preTransferStations.contains(firstTransferStation)) {
+									preTransferStations.remove(firstTransferStation);
+								}
 								if (!midTransferStations.isEmpty() && midTransferStations.contains(firstTransferStation)) {
-								    midTransferStations.remove(firstTransferStation);
+									midTransferStations.remove(firstTransferStation);
 								}
 								if (!midTransferStations.isEmpty() && midTransferStations.contains(secondTransferStation)) {
-								    midTransferStations.remove(secondTransferStation);
+									midTransferStations.remove(secondTransferStation);
+								}
+								if (!postTransferStations.isEmpty()	&& postTransferStations.contains(secondTransferStation)) {
+									postTransferStations.remove(secondTransferStation);
 								}
 
-								// 确保 postTransferStations 不包含 secondTransferStation
-								if (!postTransferStations.isEmpty() && postTransferStations.contains(secondTransferStation)) {
-								    postTransferStations.remove(secondTransferStation);
-								}
-
+								rtnMap.put("sameRoute", false);
 								rtnMap.put("preTransferStations", String.join("、", preTransferStations));
-								rtnMap.put("midTransferStations", String.join("、", midTransferStations));
-								rtnMap.put("postTransferStations", String.join("、", postTransferStations));
 								rtnMap.put("firstTransferStation", firstTransferStation);
+								rtnMap.put("midTransferStations", String.join("、", midTransferStations));
 								rtnMap.put("secondTransferStation", secondTransferStation);
+								rtnMap.put("postTransferStations", String.join("、", postTransferStations));
 								rtnMap.put("stationCount", stationNum);
 								rtnMap.put("fare", totalFare.toPlainString());
-								rtnMap.put("sameRoute", false);
 								break;
 
 							}
+							// 跳出外層迴圈
 							if (directTransferFound) {
 								break;
 							}
@@ -268,10 +255,11 @@ public class MidtermServiceImpl implements MidtermService {
 		return rtnMap;
 
 	}
-
-	private List<String> calculateRouteAndFare(Connection conn, String route, String startStation, String endStation)
+	
+	// 計算經過的站點
+	private List<String> calculateRoute(Connection conn, String route, String startStation, String endStation)
 			throws SQLException {
-//		String resultMessage = "";
+
 		List<String> metroList = new ArrayList<>();
 
 		final String SEL_SQL_SAME_ROUTE = "select STATION from STUDENT.EX_METRO where ROUTE= ? and STATION_ORDER between ? and ? order by STATION_ORDER";
@@ -280,7 +268,7 @@ public class MidtermServiceImpl implements MidtermService {
 			int startOrder = getStationOrder(conn, route, startStation);
 			int endOrder = getStationOrder(conn, route, endStation);
 
-			// 設定參數，確保 startOrder 總是小於或等於 endOrder
+			// 設定參數，確保 startOrder 總是小於 endOrder
 			pstmt.setString(1, route);
 			pstmt.setInt(2, Math.min(startOrder, endOrder)); // 找到比較小的order
 			pstmt.setInt(3, Math.max(startOrder, endOrder)); // 找到比較大的order
@@ -315,11 +303,12 @@ public class MidtermServiceImpl implements MidtermService {
 		}
 
 	}
+	
 	// 查找轉站點的方法
-
 	private List<Map<String, String>> findTransferStations(Connection conn, String route) throws SQLException {
-		final String SEL_TRANSFER_SQL = "select STATION, TRANSFER from STUDENT.EX_METRO where ROUTE= ? and TRANSFER IS NOT NULL order by STATION_ORDER";
-		List<Map<String, String>> transferStations = new ArrayList<>();
+		final String SEL_TRANSFER_SQL = "select STATION, TRANSFER from STUDENT.EX_METRO where ROUTE= ? and TRANSFER IS NOT NULL order by STATION_ORDER"; // 篩選transfer不為空
+		List<Map<String, String>> transferStations = new ArrayList<>(); // 紀錄哪站可以轉乘 [{"station":轉乘站, "transferRoute":轉乘路線}]
+		
 		try (PreparedStatement pstmt = conn.prepareStatement(SEL_TRANSFER_SQL)) {
 			pstmt.setString(1, route);
 			try (ResultSet rs = pstmt.executeQuery()) {
